54e85d500322bc1ccbd76d5dfae5de158c91c3be
Ivan <zivac.zg@hotmail.com>
2017-10-05 21:28:55 +0200

    Dodali smo VB.NET projekt

---
+++ CSharpConsole/Program.cs
@@ -0,0 +1,12 @@
+﻿
+namespace CSharpConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            System.Console.WriteLine("Zdravo narode");
+            System.Console.ReadKey(true);
+        }
+    }
+}
---
+++ VbNetConsole/Module1.vb
@@ -0,0 +1,10 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Zdravo vižual bejzik narode")
+        Console.ReadKey(True)
+
+
+    End Sub
+
+End Module
